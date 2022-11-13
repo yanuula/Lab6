b16424d2553c1a9aedaca1058702ad06a96130e1
yanuula <113615651+yanuula@users.noreply.github.com>
2022-11-13 20:56:01 +0500

    Update Lab6_Шалупова_Фт_210007.py

--- Lab6_Шалупова_Фт_210007.py
+++ Lab6_Шалупова_Фт_210007.py
@@ -1,59 +1,33 @@
-import sys
-import msvcrt as m
-def convertToInt(n):
+import numpy as np
+while True:
     try:
-        n = int(n)
-    except Exception:
-        return -1
-    return n
-#матрица
-def inputTable(table, n):
-    for i in range(n):
-        for j in range(n):
-            if (i == j):
-                table[i][j] = 1
-            if (i < j):
-                while table[i][j] == 0:
-                    temp = input("Введите отношение критерия {0} к критерию {1}: ".format(i+1, j+1))
-                    temp = convertToInt(temp)
-                    if (temp == -1) or (1 <= temp <= 9) == False: 
-                        print('Отношение должно быть целым числом от 1 до 9')
-                    else:
-                        table[i][j] = temp
-    for i in range(n):
-        for j in range(n):
-            if (i > j):
-                table[i][j] = 1/table[j][i]
-    return table
-def tableSum(table, n):
-    sum = 0
-    for i in range(n):
-        for j in range(n):
-            sum += table[i][j]
-    return sum
-def countWQ(table,n,sum):
-    columnSum = 0
-    arrayWQ  = list()
-    for i in range(n):
-        for j in range(n):
-            columnSum += table[j][i]
-        arrayWQ.append(columnSum/sum)
-    return arrayWQ
-n = 0
-#Ввод количества критериев
-while n == 0:
-    n = input("Введите количество критериев: ")
-    n = convertToInt(n)
-    if (n == -1) or (n < 1):
-        print("Количество критериев должно быть положительным целым числом")
-        n = 0
-a = [[0] * n for i in range(n)]
-a = inputTable(a,n)
-a_sum = tableSum(a,n)
-WQ = countWQ(a,n,a_sum)
-WQ.reverse()
-print("Весовые коэффициенты:", end=" ") #Вывод весовых коэффицентов
-for elem in WQ:
-    print("{0:.2f}".format(elem), end=" ")
-m.getch()
-sys.exit()
+        cr_amount = int(input("Введите количество критериев: "))
+        break
+    except ValueError:
+        print('Количество критериев должно быть положительным целым числом')
+#Матрица
+matr = np.eye(cr_amount)
+#Заполнение матрицы коэффициентами
+a = 1
+for p in range(a, cr_amount+1):
+    for q in range(a+1, cr_amount+1):
+        while True:
+            try:
+                #Заполнение каждого элемента строки матрицы
+                matr[p-1][q-1] = round(float(input('Введите отношение критериев {0}-{1}: '.format(p, q))), 3)
+                break
+            except ValueError:
+                print('Неверный ввод')
+        #Заполнение ячеек для обратного соотношения
+        matr[q-1][p-1] = round(matr[p-1][q-1]**(-1), 2)
+    a += 1
+#Суммы строк
+sum_matr = [round(sum(j),2) for j in matr]
+sum_matr1 = [round(n/sum(sum_matr), 2) for n in sum_matr]
+if (sum(sum_matr1)) != 1.0:
+    index = sum_matr1.index(max(sum_matr1))
+    k = (sum(sum_matr1)) - 1.0
+    sum_matr1[index] -= k
+print('Весовые коэффициенты: ')
+for ind in sum_matr1:
+    print(ind, end=' ')
